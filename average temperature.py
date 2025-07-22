621f79e9f74cfa341c8a6d4b7541bf231a637a70
shahil43-dev <shahilbaral123@gmail.com>
2025-07-22 08:27:15 +0545

    Add files via upload

---
+++ average temperature.py
@@ -0,0 +1,4 @@
+# Simple program to calculate average temperature
+temperatures = [25.5, 26.1, 24.8, 27.0]
+average = sum(temperatures) / len(temperatures)
+print("Average temperature:", round(average, 2))
